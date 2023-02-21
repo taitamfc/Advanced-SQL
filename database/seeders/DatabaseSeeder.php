9bf0d47136055ca53a0bc905a914c1d67bad238e
taitamfc <taitam8590@gmail.com>
2023-02-21 09:46:51 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(NHANVIEN::class);
+        $this->call(NHACUNGCAP::class);
+        $this->call(MATHANG::class);
+        $this->call(LOAIHANG::class);
+        $this->call(KHACHHANG::class);
+        $this->call(DONDATHANG::class);
+        $this->call(CHITIETDATHANG::class);
+   
     }
 }
